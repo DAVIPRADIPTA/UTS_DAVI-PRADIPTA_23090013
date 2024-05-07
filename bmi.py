fb0e942c77bdf97cd354b4ee68fcdb284d0143a1
Davi Pradipta <davipradipta456@gmail.com>
2024-05-07 16:25:52 +0700

    first commit

--- bmi.py
+++ bmi.py
@@ -3,16 +3,15 @@ def hitung_bmi ():
     tinggi_badan = float(input("masukan tinggi badan (m) : "))
 
     hasil_bmi = float(berat_badan / (tinggi_badan ** 2))
-    print (f"berat badan : {berat_badan}kg")
-    print (f"tinggi badan : {tinggi_badan}m")
+    print (f"berat badan : {berat_badan} kg")
+    print (f"tinggi badan : {tinggi_badan} m")
     print(f"Nilai bmi anda : {hasil_bmi}")
     if hasil_bmi < 18.5 :
         print("berat badan kurang")
     elif hasil_bmi >= 18.5 and hasil_bmi < 24.9 :
         print("berat badan normal")
-    elif hasil_bmi > 25 and hasil_bmi <29.9:
+    elif hasil_bmi >= 25 and hasil_bmi < 29.9:
         print("Kelebihan berat badan")
     else :
-
         print("Obesitas")
 
